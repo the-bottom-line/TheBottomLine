a5ec52700d16d4568cae892f4fc427a7226ced80
Koen <k.mann@student.fontys.nl>
2025-12-02 14:23:00 +0100

    Bugfix BO-583
    Limit websocket to reject duplicate messages until server response has been received.
    Added automatic reconnecting upon connection close.

--- src/models/NetworkManager.js
+++ src/models/NetworkManager.js
@@ -2,23 +2,26 @@ class NetworkManager {
     constructor(url) {
         this.url = url;
         this.queue = [];
-        this.isProcessing = false;
+        this.messageInTransit = false;
         this.gameManager = null;
         this.commandList = {};
-        this.connect();
+        this.connect(url);
+        this.attempts = 0;
+        this.timeoutTable = [2, 5, 15, 30, 60];
+        this.recentMessages = new Set();
     }
 
     setGameManager(gameManager) {
         this.gameManager = gameManager;
         this.commandList = {
-            "StartGame" : this.gameManager.messageStartGame.bind(this.gameManager),            
+            "StartGame" : this.gameManager.messageStartGame.bind(this.gameManager),
             "PlayersInLobby" : this.gameManager.newPlayer.bind(this.gameManager),
             "SelectingCharacters": this.gameManager.chairmanSelectCharacter.bind(this.gameManager),
             "YouDrewCard": this.gameManager.youDrewCard.bind(this.gameManager),
             "DrewCard": this.gameManager.drewCard.bind(this.gameManager),
             "YouPutBackCard": this.gameManager.youPutBackCard.bind(this.gameManager),
             "PutBackCard": this.gameManager.putBackCard.bind(this.gameManager),
-            "SelectedCharacter" : this.gameManager.receiveSelectableCharacters.bind(this.gameManager), 
+            "SelectedCharacter" : this.gameManager.receiveSelectableCharacters.bind(this.gameManager),
             "YouSelectedCharacter": this.gameManager.youSelectedCharacter.bind(this.gameManager),
             "TurnStarts": this.gameManager.turnStarts.bind(this.gameManager),
             "YouBoughtAsset": this.gameManager.youBoughtAsset.bind(this.gameManager),
@@ -39,55 +42,84 @@ class NetworkManager {
         };
     }
 
-    connect() {
-        this.connection = new WebSocket(this.url);
+    // Sets up the connection with to Websocket the websocket at url
+    connect(url) {
+        console.log("Attempting to connect to server...");
+        this.connection = new WebSocket(url);
+
+        // Incoming message protocol
         this.connection.addEventListener('message', (msg) => {
             console.log(msg);
+            this.messageInTransit = false;
+            this.recentMessages.clear();
             this.handleMessage(msg);
-
         });
 
+        // Connection opened protocol
         this.connection.addEventListener("open", () => {
             console.log("Connected");
+            this.attempts = 0;
 
             this.flushQueue();
         });
+
+        // Connection closed protocol
         this.connection.addEventListener("close", () => {
-            console.log("Connection has closed");
+            let timeout = this.timeoutTable[this.attempts];
+            console.warn("Connection has closed, retrying after " + timeout + " seconds...");
+            setTimeout(() => this.connect(this.url), 1000 * timeout);
+            if (this.attempts < this.timeoutTable.length - 1) {
+                this.attempts += 1;
+            }
         })
     }
 
+    // Sends all messages stored in queue
     flushQueue() {
         this.queue.forEach(msg => {this.connection.send(msg);});
+        this.queue = [];
     }
 
+    // Couples a received message to corresponding command,
+    // Echoes and otherwise ignores the received command if not found
     handleMessage(msg) {
         let parsedMessage = JSON.parse(msg.data);
         let invokedCommand = this.commandList[parsedMessage.action];
         if (invokedCommand) {
             invokedCommand(parsedMessage.data);
+        } else {
+            console.warn("Unknown command:" + parsedMessage.action);
         }
     }
 
+    // Attempts to send a message, if connection is closed will store messages in queue
+    // Will discard duplicate messages or messages sent before a response has been returned by the server
     sendMessage(data) {
-        console.log("Sending packet: " + data);
         if (this.connection.readyState == WebSocket.OPEN) {
+            // Catch and reject duplicate messages
+            if (this.messageInTransit || this.recentMessages.has(data)) {
+                console.error("Rejected duplicate message: " + data);
+                return;
+            }
+
+            this.messageInTransit = true;
+            this.recentMessages.add(data);
             this.connection.send(data);
         } else {
             this.queue.push(data);
         }
     }
 
+    // Creates a structured message with a given command and data
     sendCommand(command, data) {
-        this.packet = {
+        let packet = {
             "action" : command,
             "data" : data
         }
-        this.jsonData = JSON.stringify(this.packet, null, 0);
-        console.log(this.jsonData);
-        this.sendMessage(this.jsonData);
+        let jsonData = JSON.stringify(packet, null, 0);
+        console.log(jsonData);
+        this.sendMessage(jsonData);
     }
-
 }
 
 export default NetworkManager;
