888c6c61da4993466863ad91bc8c51fda1b31f37
oxey <lucoerlemans37@gmail.com>
2025-12-05 12:12:53 +0100

    make `Liability.ts` valid typescript

--- src/models/Liability.ts
+++ src/models/Liability.ts
@@ -1,11 +1,13 @@
-import GeneralCard from './GeneralCard'
+import GeneralCard from './GeneralCard.js'
 
 class Liability extends GeneralCard {
-    constructor(title, gold, texturePath) {
+    title: string;
+    gold: number;
+    
+    constructor(title: string, gold: number, texturePath: string) {
         super(texturePath);
         this.title = title;
         this.gold = gold;
-        
     }
 }
 
