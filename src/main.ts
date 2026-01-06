9356dabd1df5ecccad8dfe5ed253debd56ccb42b
Pepijn <437675@student.fontys.nl>
2026-01-06 12:15:18 +0100

    added env variable for backend url

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import GameManager from "./models/GameManager.js";
 import GameState from "./models/GameState.js";
 import UIManager from "./models/UIManager.js";
 import NetworkManager from "./models/NetworkManager.js";
+import dotenv from 'dotenv'; 
 
 (async () => {
     const app = new Application();
@@ -16,9 +17,14 @@ import NetworkManager from "./models/NetworkManager.js";
     app.canvas.style.position = "absolute";
     document.body.appendChild(app.canvas);
 
+    dotenv.config();  // Load environment variables from .env file 
+    let url = process.env.BOTTOM_ONLINE_BACKEND_URL;
+    if (url === undefined){
+        url = "http://localhost:3000/websocket"
+    }
     const gameState = new GameState();
     const uiManager = new UIManager(app);
-    const networkManager = new NetworkManager(' http://localhost:3000/websocket'); //192.168.67.151
+    const networkManager = new NetworkManager(url); //192.168.67.151
     const gameManager = new GameManager(gameState, uiManager, networkManager);
 
     networkManager.setGameManager(gameManager);
