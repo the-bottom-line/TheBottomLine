02c4dc002d7fe354bbb571d211b58ef0696ff0bd
Pepijn <437675@student.fontys.nl>
2026-01-07 11:15:12 +0100

    updated docker file for env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
-import 'vite/client'
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BOTTOM_ONLINE_BACKEND_URL: string
+  readonly VITE_TEST: Number
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
